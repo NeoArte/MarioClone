32e794d366d67ca8df716fc61c021029bdcafd74
NeoArte <nicolasdestito74@gmail.com>
2021-11-21 16:43:40 -0300

    Flagpole for the level end created as well as it's signal

--- Platformer/src/Actors/Enemies/BaseEnemy/Scripts/Enemy.gd
+++ Platformer/src/Actors/Enemies/BaseEnemy/Scripts/Enemy.gd
@@ -4,6 +4,7 @@ class_name BaseEnemy
 export var fire_immunity: bool = false
 
 func _ready() -> void:
+	# set physics process false and dsable physics process on VisibilityEnabler
 	_velocity.x = -max_spd.x
 
 func _physics_process(delta: float) -> void:
---
+++ Platformer/src/Objects/Flagpole/Flagpole.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+signal player_reached_end(level_to_move)
+
+export(String, FILE) var NEXT_LEVEL: String = ""
+
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if NEXT_LEVEL != "":
+		# A signal to the level node will be send with what the next level is
+		emit_signal('player_reached_end', NEXT_LEVEL) 
