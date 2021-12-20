0cd4bd9c606b48e207369d36fccd8ac1d7182fd0
NeoArte <nicolasdestito74@gmail.com>
2021-12-20 17:05:00 -0300

    Organazined variables in player code

--- Platformer/src/Actors/Player/Scripts/Player.gd
+++ Platformer/src/Actors/Player/Scripts/Player.gd
@@ -2,33 +2,32 @@ extends Actor
 
 signal player_died
 
-var direction: Vector2 = Vector2.ZERO
-
 export var min_speed = 50
-
 export var max_jump_force = 700
-var jump_force = max_jump_force
-
-export var jump_press_time := 0.5
-var jump_pressed: bool = false
-export var coyote_time := 0.1
-var can_jump: bool = false
-
 export var stomp_impulse: float = 500
+export var jump_press_time := 0.5
+export var coyote_time := 0.1
+
+
+var direction: Vector2 = Vector2.ZERO
+var jump_force = max_jump_force
+var jump_pressed: bool = false
+var can_jump: bool = false
+var impulsed: bool = false
+var dead: bool = false
 
 onready var stomp_detector: Area2D = $StompDetector
 onready var anim_player: AnimationPlayer = $AnimationPlayer
 
-var dead: bool = false
 
 func _on_StompDetector_area_entered(area: Area2D) -> void:
 	print("Player: ", stomp_detector.global_position.y, "\nEnemy: ", area.global_position)
 	if stomp_detector.global_position.y > area.global_position.y:
 		return
-	print("IMPULSE DATA\n=======")
-	print("Enemy Y: ", area.global_position.y)
-	print("Player Y: ", stomp_detector.global_position.y)
+	print("IMPULSE DATA\n=======\n", "Enemy Y: ", area.global_position.y,\
+	"\nPlayer Y: ", stomp_detector.global_position.y)
 	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
+	impulsed = true
 
 
 func _on_EnemyDetector_body_entered(_body: Node) -> void:
@@ -40,12 +39,13 @@ func _physics_process(delta: float) -> void:
 	
 	if is_on_floor():
 		can_jump = true
+		impulsed = false
 	if !is_on_floor():
 		activate_coyote_time()
 	
 	print(can_jump)
 	
-	var is_jump_interrupted = Input.is_action_just_released("jump") and _velocity.y < 0.0
+	var is_jump_interrupted = Input.is_action_just_released("jump") and _velocity.y < 0.0 and !impulsed
 	direction = get_direction()
 	
 	_velocity = calculate_move_velocity(_velocity, direction, acceleration, deacceleration, \
@@ -55,22 +55,8 @@ func _physics_process(delta: float) -> void:
 	_velocity = move_and_slide_with_snap(_velocity, snap, FLOOR_NORMAL, true)
 #	print(_velocity.x)
 
-func activate_coyote_time() -> void:
-	yield(get_tree().create_timer(coyote_time), "timeout")
-	can_jump = false
-
-func remember_jump_time() -> void:
-	yield(get_tree().create_timer(0.1), "timeout")
-	jump_pressed = false
-
 func get_direction() -> Vector2:
 	var x_dir = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	
-	# For the jump to occur the player must be on the ground (is_on_floor), the
-	# jump key must just have been pressed and it cannot have been released.
-#	var y_dir = -Input.get_action_strength("jump") if is_on_floor() and \
-#	Input.is_action_just_pressed("jump") else 0.0
-#	jump_pressed = Input.is_action_just_pressed("jump")
 	var y_dir = 0.0
 	var input_jump = Input.is_action_just_pressed("jump")
 	if can_jump:
@@ -81,6 +67,16 @@ func get_direction() -> Vector2:
 			y_dir = -1.0
 	
 	return Vector2(x_dir, y_dir)
+
+
+func activate_coyote_time() -> void:
+	yield(get_tree().create_timer(coyote_time), "timeout")
+	can_jump = false
+
+
+func remember_jump_time() -> void:
+	yield(get_tree().create_timer(0.1), "timeout")
+	jump_pressed = false
 
 
 func calculate_move_velocity(
@@ -108,7 +104,7 @@ func calculate_move_velocity(
 		velocity.x = lerp(_velocity.x, 0, (deacceleration*delta)/1.5)
 	# Accelerating
 	elif direction.x != 0:
-		var vel_dir := 1 if not abs(velocity.x) - velocity.x else -1
+		var vel_dir := sign(velocity.x)
 		vel_dir = direction.x if not velocity.x else vel_dir
 #		print("Vx: %s\nMd: %s" % [velocity.x, min_speed*vel_dir])
 		var is_accel = vel_dir == direction.x or abs(velocity.x) < 20
@@ -125,6 +121,8 @@ func calculate_move_velocity(
 	if direction.y != 0.0:
 		velocity.y -= max_jump_force
 		jump_pressed = false
+	if is_jump_interrupted:
+		velocity.y = 0.0
 	return velocity
 
 
