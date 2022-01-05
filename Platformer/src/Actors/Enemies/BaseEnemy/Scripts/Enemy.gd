fdddbcbeea6fbf7c21a51999e167c1db28cf5a83
NeoArte <nicolasdestito74@gmail.com>
2022-01-05 15:31:14 -0300

    Mush animations added and some other smaller enemy tweeks

--- Platformer/src/Actors/Enemies/BaseEnemy/Scripts/Enemy.gd
+++ Platformer/src/Actors/Enemies/BaseEnemy/Scripts/Enemy.gd
@@ -1,14 +1,21 @@
 extends Actor
 class_name BaseEnemy
 
+onready var anim_player: AnimationPlayer = $AnimationPlayer
+onready var pivot: Node2D = $Pivot
+
 export var fire_immunity: bool = false
 
 func _ready() -> void:
 	set_physics_process(false)
 	_velocity.x = -max_spd.x
+	anim_player.play("walk")
 
 func _physics_process(_delta: float) -> void:
-	_velocity.x *= -1 if is_on_wall() else 1
+	if is_on_wall():
+		_velocity.x *= -1
+		pivot.scale.x *= -1
+		
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
 
 
